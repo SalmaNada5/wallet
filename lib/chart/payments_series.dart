b6898d452ee5cc16f3b47e99f5935d062b9986cb
salma <salma.nada.fci@gmail.com>
2022-01-04 18:48:46 +0200

    init

---
+++ lib/chart/payments_series.dart
@@ -0,0 +1,10 @@
+// import 'package:flutter/material.dart';
+// import 'package:charts_flutter/flutter.dart' as charts;
+
+// class PaymentSeries {
+//   final String? day;
+//   final int? money;
+//   final charts.Color? barColor;
+//   PaymentSeries(
+//       {@required this.day, @required this.money, @required this.barColor});
+// }
